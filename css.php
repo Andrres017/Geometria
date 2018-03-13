08a342a015203da7d09c488725847785c8b8eda0
Andres felipe (daratot) <Andrres017@gmail.com>
2018-03-13 12:15:58 -0500

    en proceso

---
+++ css.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap-grid.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap-reboot.css">
---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<?php include 'css.php'; ?>
+		<title>Bienvenido</title>
+
+</head>
+<body style="background-color: #e1bee7" >
+<br><br>
+<div class="container" >
+	<center><h2>Unidad Didáctica Para La Enseñanza De Las Secciones Cónicas Como Lugar Geométrico</h2></center>
+  <div class="row" style="position:  absolute; top:35%;position: absolute; left: 10%;position: absolute;right: 10% "> 
+  	<div class="col">
+      <center>
+      	<button type="button" class="btn btn-info btn-lg btn-block">Actividad 1</button>
+      </center>
+    </div>
+    <div class="col">
+      <center>
+      	<button type="button" class="btn btn-info btn-lg btn-block">Actividad 2</button>
+      </center>
+    </div>
+    <div class="col">
+      <center>
+      	<button type="button" class="btn btn-info btn-lg btn-block">Actividad 3</button>
+      </center>
+    </div>
+  </div>
+  <div class="row" style="position:  absolute; top:45%;position: absolute; left: 20%;position: absolute;right: 20% "> 
+  	<div class="col">
+      <center>
+      	<button type="button" class="btn btn-info btn-lg btn-block">Actividad 4</button>
+      </center>
+    </div>
+    <div class="col">
+      <center>
+      	<button type="button" class="btn btn-info btn-lg btn-block">Actividad 5</button>
+      </center>
+    </div>
+  </div>
+  <div class="row" style="position:  absolute; top:55%;position: absolute; left: 10%;position: absolute;right: 10% "> 
+  	<div class="col">
+      <center>
+      	<button type="button" class="btn btn-info btn-lg btn-block">Actividad 6</button>
+      </center>
+    </div>
+    <div class="col">
+      <center>
+      	<button type="button" class="btn btn-info btn-lg btn-block">Actividad 7</button>
+      </center>
+    </div>
+    <div class="col">
+      <center>
+      	<button type="button" class="btn btn-info btn-lg btn-block">Actividad 8</button>
+      </center>
+    </div>
+  </div>
+</div>
+
+
+</body>
+<?php include 'js.php';?>
+</html>
